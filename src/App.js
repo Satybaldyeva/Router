ce88c8b0e0fe25a1a055d51893622695d020b3ba
Kanykei <kanisatybaldyeva@gmail.com>
2023-08-15 23:06:57 +0600

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 
+import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import { Products } from './pages/Products';
+import { MyCart } from './pages/MyCart';
+import { MyOrders } from './pages/MyOrders';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+   
+      <>
+         <header>
+          <b>iStore</b>
+          <nav>
+          <Link to={'/'}></Link>
+          <Link to={'/products'}>Products</Link>
+          <Link to={'/my-cart'}>My Cart</Link>
+          <Link to={'/my-orders'}>My Orders</Link>
+          </nav>
+       </header>
+
+       <Routes>
+       <Route path='/' element={<div></div>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/my-cart' element={<MyCart/>}/>
+        <Route path='/my-orders' element={<MyOrders/>}/>
+       </Routes>
+      </>
+     
+   
   );
 }
 
